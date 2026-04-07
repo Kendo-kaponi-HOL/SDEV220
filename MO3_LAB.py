2f35b9ac18c78a7f4936414f4168034deefb48b0
Vgusiradoz <polloman3344@gmail.com>
2026-04-06 22:35:21 -0400

    Fixed 'None' message in terminal, removed the test comments, added new formatting

--- MO3_LAB.py
+++ MO3_LAB.py
@@ -14,24 +14,28 @@ class Automobile(Vehicle):
         self.roof = roof
     
     def get_car_info(self):
+        print("-"*50)
+        print()
         print(f"Vehicle type: {self.type}\nYear: {self.year}\nMake: {self.make}\nModel: {self.model}\nNumber of doors: {self.doors}\nType of roof: {self.roof}")
-    
+        print()
+        print("-"*50)
 
 def add_car():
-    vehicle_type = "Truck"      # vehicle_type = input("\nEnter vehicle type: \n")
-    vehicle_year = "2002"       # vehicle_year = input("\nEnter vehicle year: \n")
-    vehicle_make = "Toyota"     # vehicle_make = input("\nEnter vehicle make: \n")
-    vehicle_model = "Lexus"     # vehicle_model = input("\nEnter vehicle model: \n")
-    vehicle_doors = 4                            # while True:
-    vehicle_roof = "solid"                            #     try:
-                                #         vehicle_doors = int(input("\nEnter vehicle doors 2-4: \n"))
-                                #         if 2 <= vehicle_doors <= 4:
-                                #             break
-                                #         else:
-                                #             print("Please enter a number between 2 and 4")
-                                #     except ValueError:
-                                #         print("You have entered an incorrect value, please enter a number between 2 and 4")
-                                # vehicle_roof = input("\nEnter vehicle roof type (solid or sun roof): \n")
+    vehicle_type = input("\nEnter vehicle type: \n")            # vehicle_type = "Truck"
+    vehicle_year = input("\nEnter vehicle year: \n")            # vehicle_year = "2002"       
+    vehicle_make = input("\nEnter vehicle make: \n")            # vehicle_make = "Toyota"
+    vehicle_model = input("\nEnter vehicle model: \n")          # vehicle_model = "Lexus"
+    while True:                                                 # vehicle_doors = 4
+        try:                                                    # vehicle_roof = "solid"
+            vehicle_doors = int(input("\nEnter vehicle doors 2-4: \n"))
+            if 2 <= vehicle_doors <= 4:
+                break
+            else:
+                print("Please enter a number between 2 and 4")
+        except ValueError:
+            print("You have entered an incorrect value, please enter a number between 2 and 4")
+    vehicle_roof = input("\nEnter vehicle roof type (solid or sun roof): \n")
+    print()
     
     instance = Automobile(vehicle_type, vehicle_year, vehicle_make, vehicle_model, vehicle_doors, vehicle_roof)
     return instance
@@ -45,7 +49,7 @@ if __name__ == "__main__":
             print()
             if decision == 1:
                 my_car = add_car()
-                print(my_car.get_car_info())
+                my_car.get_car_info()
                 print("\nProgram on cooldown :)\n")
                 time.sleep(3)
             elif decision == 2:
